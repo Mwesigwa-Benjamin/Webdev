4830849b4ad2dced42c4acb9b32a5bd677f60c88
Mwesigwa-Benjamin <mbenmwesigwa@gmail.com>
2025-02-05 00:39:15 +0300

    react native profile

--- index.tsx
+++ index.tsx
@@ -1,35 +1,53 @@
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '@/components/HomeScreen';
-import TrackOne from '@/components/TrackOne';
-import TrackOthers from '@/components/TrackOthers';
-import TrackTwo from '@/components/TrackTwo';
-import TrackThree from '@/components/TrackThree';
-import TrackFour from '@/components/TrackFour';
-import TrackFive from '@/components/TrackFive';
-import TrackSix from '@/components/TrackSix';
+import ProfileScreen from '@/components/profileScreen';
+import AccountScreen from '@/components/MathPaper';
+import FirstPage from '@/components/firstPage';
+import { LogIn } from '@/components/LogIn';
+import HomePage from '@/components/homePage';
+import NotifyScreen from '@/components/NotifyScreen';
+import MathPaper from '@/components/MathPaper';
+import PhysicsPaper from '@/components/PhysicsPaper';
+import BiologyPaper from '@/components/BiologyPaper';
+
 
 
 const Stack = createNativeStackNavigator();
 
-
-export default function Index(){
+export default function HomeScreen(){
 return(
-  <Stack.Navigator initialRouteName='HomeScreen'>
-    <Stack.Screen name='HomeScreen' component={HomeScreen} />
-    <Stack.Screen  name='TrackOne' component={TrackOne}/>
-    <Stack.Screen  name='TrackOthers' component={TrackOthers}/>
-    <Stack.Screen  name='TrackTwo' component={TrackTwo}/>
-    <Stack.Screen  name='TrackThree' component={TrackThree}/>
-    <Stack.Screen  name='TrackFour' component={TrackFour}/>
-    <Stack.Screen  name='TrackFive' component={TrackFive}/>
-    <Stack.Screen  name='TrackSix' component={TrackSix}/>
-  </Stack.Navigator>
 
+<Stack.Navigator initialRouteName='FirstPage'>
+  <Stack.Screen name='FirstPage' component={FirstPage}/>
+  <Stack.Screen name='LogIn' component={LogIn}/>
+  <Stack.Screen name='HomePage' component={HomePage}/>
+  <Stack.Screen name='NotifyScreen' component={NotifyScreen}/>
+  <Stack.Screen name='ProfileScreen' component={ProfileScreen}/>
+  <Stack.Screen name='MathPaper' component={MathPaper}/>
+  <Stack.Screen name='PhysicsPaper' component={PhysicsPaper}/>
+  <Stack.Screen name='BiologyPaper' component={BiologyPaper}/>
+</Stack.Navigator>
 
-  
-  
 );
-
 }
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
